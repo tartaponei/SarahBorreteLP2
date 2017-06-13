0bf360f5b7946a44f98980cd5efc5ddebad6982c
Sarah Borrete <sarahborretti@gmail.com>
2017-06-13 09:12:22 -0300

    Parte 1 exercício Objetos no Plano

---
+++ Objetos-No-Plano/Objeto2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    public class Objeto2D
+    {
+        public int x, y;
+
+        public void AndarParaADireita()
+        {
+            x += 1;
+        }
+        public string Coordenadas()
+        {
+            return String.Format("{0}, {1}", x, y);
+        }
+    }
---
+++ Objetos-No-Plano/Objetos-No-Plano/Objeto2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    public class Objeto2D
+    {
+        public int X, Y;
+
+        public void MoverParaDireita()
+        {
+            X += 1;
+        }
+        public string Coordenadas()
+        {
+            return String.Format("{0}, {1}", X, Y);
+        }
+    }
