e19223905fbcac605883458326c12d2e74d72cee
Sarah <sarahborretti@gmail.com>
2017-06-13 22:56:18 -0300

    Exercício Competição de Corrida

---
+++ Competição-de-Corrida/Competição-de-Corrida/Competidor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competição_de_Corrida
+{
+    public class Competidor
+    {
+        public double t1;
+        public double t2;
+        public double t3;
+
+        public double TempoOficial()
+        {
+            if (t1 < t2 && t1 < t3)
+                return t2 + t3;
+
+            else if (t2 < t1 && t2 < t3)
+                return t1 + t3;
+
+            else
+                return t1 + t2;
+        }
+    }
+}
