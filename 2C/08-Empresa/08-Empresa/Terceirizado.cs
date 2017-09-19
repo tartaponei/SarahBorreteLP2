8e52ca7770301323163e9e8f92b2748788687118
sarah <sarahborretti@gmail.com>
2017-09-19 15:54:49 -0300

    Exercício da empresa (incompleto)

---
+++ 2C/08-Empresa/08-Empresa/Terceirizado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Empresa
+{
+    class Terceirizado
+    {
+        public string Setor;
+        public int DataEntrada;
+        private string EmpMatriz;
+
+        private int numRegistro;
+        private int salario;
+        public string nome;
+        public int cpf { get; set; }
+        public string email;
+
+        public Terceirizado(string x, string y, int z)
+        {
+            Setor = x;
+            EmpMatriz = y;
+            numRegistro = z;
+        }
+    }
+}
